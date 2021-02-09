4234030fa57179c350f1ac894cef3b74fee8257a
shijie1xu <43320663+shijie1xu@users.noreply.github.com>
2021-02-09 15:47:16 -0500

    POST edit, DELETE and reorder function for admin categories

--- src/main/java/com/shijie/cmsshoppingcart/models/CategoryRepository.java
+++ src/main/java/com/shijie/cmsshoppingcart/models/CategoryRepository.java
@@ -3,9 +3,11 @@ package com.shijie.cmsshoppingcart.models;
 import com.shijie.cmsshoppingcart.models.data.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     Category findByName(String name);
 
-
+    List<Category> findAllByOrderBySortingAsc();
 }
